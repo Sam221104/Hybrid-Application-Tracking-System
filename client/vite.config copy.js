5751fdfbbc25cc7f2eca3a5c6f00b2d8a5c4e6f9
sam221104 <samsylvester1206@gmail.com>
2025-10-22 08:59:05 +0530

    Remove API proxy configuration from Vite server settings

--- client/vite.config copy.js
+++ client/vite.config copy.js
@@ -1,11 +1,5 @@
 export default {
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://hybrid-application-tracking-system-6s0h.onrender.com',
-        changeOrigin: true,
-        secure: false
-      }
-    }
+    
   }
 }
